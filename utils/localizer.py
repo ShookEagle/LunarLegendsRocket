c869b00fdb6f8f25efc2a0d8f173f6b2db5837ee
ShookEagle <shookeagle@gmail.com>
2025-03-22 10:02:09 -0500

    Signleton Localizer instance

--- utils/localizer.py
+++ utils/localizer.py
@@ -1,24 +1,24 @@
 import json
 import os
-from config.settings import LANG_PATH
-
+from config.settings import LANG_PATH, LANGUAGE
 
 def load_language_file(language):
     file_path = os.path.join(LANG_PATH, f"{language}.json")
     with open(file_path, "r", encoding="utf-8") as f:
-            return json.load(f)
-
+        return json.load(f)
 
 class Localizer:
-    def __init__(self, language="en"):
+    def __init__(self, language=LANGUAGE):
         self.language = language
         self.locale = load_language_file(language)
 
     def print_local(self, key, **kwargs):
-        localized_message = self.locale.get(key, f"Missing message for key: {key}")
-        print(localized_message.format(**kwargs))
+        message = self.locale.get(key, f"Missing message for key: {key}")
+        print(message.format(**kwargs))
 
     def print_local_error(self, key, **kwargs):
-        localized_message = self.locale.get(key, f"Missing message for key: {key}")
-        error_message = localized_message.replace("%error%", self.locale.get("error"))
-        print(error_message.format(**kwargs))
+        message = self.locale.get(key, f"Missing message for key: {key}")
+        print("ERROR: " + message.format(**kwargs))
+
+# Global shared instance
+localizer = Localizer()
