fcafc35ce3ad1f4e26fb11ea857724e2f3cd0076
ShookEagle <shookeagle@gmail.com>
2025-05-06 09:47:33 -0500

    zero altitude at init

--- sensors/bmp.py
+++ sensors/bmp.py
@@ -7,9 +7,10 @@ class Altimeter:
         i2c = board.I2C()
         self.bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
         self.bmp.sea_level_pressure = 1013.0
+        self.startAltitude = self.bmp.altitude
 
     def read_altitude(self):
-        return self.bmp.altitude
+        return self.bmp.altitude - self.startAltitude
 
     def read_bmp_temperature(self):
         return self.bmp.temperature
